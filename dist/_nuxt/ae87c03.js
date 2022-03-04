e345aada6b66127d221cf79fce5a3935f35a4e22
Yuanita7149 <94151011+Yuanita7149@users.noreply.github.com>
2022-03-04 11:50:23 +0700

    Add files via upload

---
+++ dist/_nuxt/ae87c03.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{281:function(n,e,t){"use strict";t.r(e);var l=t(62),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("main")}),[],!1,null,null,null);e.default=component.exports}}]);
